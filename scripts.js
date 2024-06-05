a177aafad3178f28da6b764404127bd76ce6cdc4
Mihai Niculae <85626030+mihainrs@users.noreply.github.com>
2024-06-05 19:12:45 +0300

    html + css done!

---
+++ scripts.js
@@ -0,0 +1,7 @@
+let burgermenu = document.querySelector(".header");
+let borgir = document.querySelector(".burger");
+let  = 
+
+borgir.addEventListener("click", ()=>{
+      burgermenu.style.display = "flex";
+})
