2cd007b7f36a1be8fb0b1edccc9e1c90351316ea
Mihai Niculae <85626030+mihainrs@users.noreply.github.com>
2024-06-06 14:35:49 +0300

    done :dolphin:

--- scripts.js
+++ scripts.js
@@ -1,7 +1,15 @@
 let burgermenu = document.querySelector(".header");
 let borgir = document.querySelector(".burger");
-let  = 
+let body = document.body;
 
-borgir.addEventListener("click", ()=>{
-      burgermenu.style.display = "flex";
+borgir.addEventListener("click", () => {
+      if (burgermenu.style.display != "flex") {
+            burgermenu.style.display = "flex";
+            body.style.backgroundColor = "rgba(0, 0, 0, 30%)";
+            borgir.src = "assets/images/icon-menu-close.svg";
+      } else{
+            burgermenu.style.display = "none";
+            body.style.backgroundColor = "white";
+            borgir.src = "assets/images/icon-menu.svg"
+      }
 })
